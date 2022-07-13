1d214d8469a99a51b5f51d0ab3454ad1e6e0f389
tien <tiendv.icebear>
2022-07-13 23:37:44 +0700

    error_B_tree

---
+++ Super Stickman/Assets/Scrips/EnemyAttack.cs
@@ -0,0 +1,11 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class EnemyAttack : Action
+{
+    public override void OnAwake()
+    {
+        EnemyControoller.instance.BtSkill1();
+    }
+}
